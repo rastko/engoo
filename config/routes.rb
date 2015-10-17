51711c924fd9a6de7148fc652e808a541e5d1557
rastko <ludi.rashid@gmail.com>
2015-10-18 01:45:54 +0200

    Add root routes for authenticated/unauthenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :members
+
+  authenticated :member do
+    root to: "dashboard#index", as: :authenticated_root
+  end
+
+  devise_scope :member do
+    get "/", to: "devise/sessions#new"
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
